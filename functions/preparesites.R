b6f7fdd44bcb7cb5ad9ea319d18d098bbcb98873
Ryan Knight <rk1007@york.ac.uk>
2021-01-31 17:48:07 +0000

    Working function to structure for deepphos (infinity hours later)

---
+++ functions/preparesites.R
@@ -0,0 +1,22 @@
+#### Prepare Sites Function ####
+# Selects data to be used in deepphos which is altered often.
+# sp and caspase should be a list of criteria.
+# Parameters ordered to be pipe friendly.
+
+prepare_sites <- function(df, sites, caspase="ALL", sp="ALL") {
+
+  
+  #Written to tolerate additional caspases and species if the input data is expanded.
+  if(caspase != "ALL") {
+    df <- df %>% filter(cleaved_by %in% caspase)
+  }
+  if(sp != "ALL") {
+    df <- df %>% filter(species %in% sp)
+  }
+
+  new_data <- df %>% filter(residue %in% sites) 
+  return(new_data)
+  
+}
+
+
